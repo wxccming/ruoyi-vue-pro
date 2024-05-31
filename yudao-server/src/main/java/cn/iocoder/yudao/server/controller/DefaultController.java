12422fe5c1866fea43e2a6a2e778f25dd2e7179a
cuiming <442404685@qq.com>
2024-05-31 12:22:10 +0800

    去除敏感信息配置

--- yudao-server/src/main/java/cn/iocoder/yudao/server/controller/DefaultController.java
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/controller/DefaultController.java
@@ -21,20 +21,6 @@ public class DefaultController {
                 "[工作流模块 yudao-module-bpm - 已禁用][参考 https://doc.iocoder.cn/bpm/ 开启]");
     }
 
-    @RequestMapping("/admin-api/mp/**")
-    public CommonResult<Boolean> mp404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[微信公众号 yudao-module-mp - 已禁用][参考 https://doc.iocoder.cn/mp/build/ 开启]");
-    }
-
-    @RequestMapping(value = {"/admin-api/product/**", // 商品中心
-            "/admin-api/trade/**", // 交易中心
-            "/admin-api/promotion/**"})  // 营销中心
-    public CommonResult<Boolean> mall404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[商城系统 yudao-module-mall - 已禁用][参考 https://doc.iocoder.cn/mall/build/ 开启]");
-    }
-
     @RequestMapping("/admin-api/erp/**")
     public CommonResult<Boolean> erp404() {
         return CommonResult.error(NOT_IMPLEMENTED.getCode(),
@@ -52,11 +38,4 @@ public class DefaultController {
         return CommonResult.error(NOT_IMPLEMENTED.getCode(),
                 "[报表模块 yudao-module-report - 已禁用][参考 https://doc.iocoder.cn/report/ 开启]");
     }
-
-    @RequestMapping(value = {"/admin-api/pay/**"})
-    public CommonResult<Boolean> pay404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[支付模块 yudao-module-pay - 已禁用][参考 https://doc.iocoder.cn/pay/build/ 开启]");
-    }
-
 }
